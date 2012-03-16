c9920719abff47c396785036cdb77b2375157b07
Michal Nowotarski <novotarq@gmail.com>
2012-03-16 03:56:27 +0100

    initial commit with rspec, database cleaner, factory girl and mongoid

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuipperDemo::Application.config.secret_token = 'd7390412b32300f54c0c16d3099dab9b4ee0bf050538089c2b3d62b7b7f07f38e7b1428219c8d6645fa7dd072d2a74a8b7c249d6575fb98bffbe9ab4a9b31a03'
