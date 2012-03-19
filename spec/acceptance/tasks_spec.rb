be7680ce06ee437360597435208e33e5c77384b5
Michal Nowotarski <novotarq@gmail.com>
2012-03-19 02:38:53 +0100

    more acceptance specs

--- spec/acceptance/tasks_spec.rb
+++ spec/acceptance/tasks_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe "tasks", :type => :request do
-  before :each do
-    #FactoryGirl.create(:overdue_task)
-    #FactoryGirl.create(:completed_task)
-    #FactoryGirl.create(:todo_task)
-  end
 
   describe "create a task so that I will not forget something to do" do
     it "#good path" do
@@ -53,7 +48,7 @@ describe "tasks", :type => :request do
       find('.submit_button').click
 
       page.current_path.should == tasks_path
-      page.has_content?('my task').should be true      
+      page.has_content?('my task').should be true
     end
 
     it "#bad path" do
@@ -63,8 +58,50 @@ describe "tasks", :type => :request do
       fill_in 'Title', with: ''
       find('.submit_button').click
 
-      page.has_content?("Title can't be blank").should be true   
+      page.has_content?("Title can't be blank").should be true
     end    
+  end
+
+  describe "I want to mark a task as done so that I can distinguish incomplete tasks from complete ones" do
+    before do
+      FactoryGirl.create(:todo_task)
+    end
+
+    it "#good path" do
+      visit '/'
+      find('#task_completed').click
+
+      page.has_content?('Completed').should be true
+    end
+  end
+
+
+  describe "I want to see all tasks which didn't meet deadline as of today" do
+    before do
+      FactoryGirl.create(:overdue_task)
+    end
+
+    it "#good path" do
+      visit '/'
+
+      page.has_content?('Overdue').should be true
+    end
+  end
+
+  describe "I want to delete a task so that I can remove task which is not a task any more", :js => true do
+    before do
+      FactoryGirl.create(:todo_task)
+    end
+
+    it "#good path" do
+      visit '/'
+
+      click_link 'Destroy'
+      #TODO it's the simplest (and slow) way to do that, think about https://github.com/thoughtbot/capybara-webkit/issues/84
+      page.driver.browser.switch_to.alert.accept
+
+      page.has_content?('Todo').should be false
+    end
   end
 
 end
